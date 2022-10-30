fd9cc3c3e856c7b0bfd0952c4d4e5e0b52e8ac13
Magdalena <mkceva@gmail.com>
2022-10-30 22:18:20 +0100

    corrections

--- src/main/java/guru/springframework/sfgdi/services/DogPetService.java
+++ src/main/java/guru/springframework/sfgdi/services/DogPetService.java
@@ -6,7 +6,7 @@ import org.springframework.context.annotation.Profile;
  * Created by jt on 12/28/19.
  */
 @Profile({"dog", "default"})
-public class DogPetService {
+public class DogPetService implements  PetService{
     public String getPetType(){
         return "Dogs are the best!";
     }
